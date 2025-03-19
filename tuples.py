d04a611dc6df2904facc3ae858dcd2fbdcc7a07d
Chaser-254 <esibitaremmanuel316@gmail.com>
2025-03-19 16:42:37 +0300

    tuples in python

---
+++ tuples.py
@@ -0,0 +1,12 @@
+#tuples are similar to lists but they are immutable
+#tuples are defined using parentheses
+#tuples are faster than lists
+#tuples are used when the data cannot be changed
+
+var1 = (1,2,3,4)
+print(var1)
+
+var2 = ("Python", "Swift", "Java")
+print(var2[0])
+print(var2[1])
+print(var2[2])
